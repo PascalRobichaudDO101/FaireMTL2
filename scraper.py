8466f84ae58bb18b20f84b3dbe6aad6aeeaad704
Hackons la corruption <pascal.robichaud.do101@gmail.com>
2015-07-13 20:39:57 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,30 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import pyquery
+import re
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+ROOT = 'https://fairemtl.ca'
+dom = pyquery.PyQuery('%s/fr/projets' % ROOT)
+pages = int(dom.find('.pager-last a').attr('href').split('%2C')[-1])
+
+urls = []
+for page in range(0, pages + 1):
+    dom = pyquery.PyQuery('%s/fr/projets?page=0,%s' % (ROOT, page))
+    urls.extend("%s%s" % (ROOT, a.attrib['href']) for a in dom.find('h3 a'))
+
+for compteur, url in enumerate(urls):
+    dom = pyquery.PyQuery(url)
+    print("numero: %s" % (compteur + 1))
+    print("url: %s" % url)
+    print("projet: %s" % dom.find('h1').text())
+    # description est parfois des <span>, parfois des <p>
+    span = dom.find('.pane-node-body span')
+    if span:
+        description = span[-1].text
+    else:
+        p = dom.find('.pane-node-body p')
+        description = " ".join([e.text for e in p])
+    print("description: %s" % description)
+    print("nombre_commentaires: %s" % re.findall(r'[0-9]+', dom.find('#tabs-0-footer li.last a')[0].text)[0])
+    print("nombre_abonnes: %s" % dom.find('.js-subscribe_section_content span.count').text())
+    print( "nombre_appuis: %s" % dom.find('.js-support_project span.count').text())
